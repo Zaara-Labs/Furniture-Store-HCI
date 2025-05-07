b9a9de16da50d448c1ccb6340ada125022280118
scssandanayake <114943338+scssandanayake@users.noreply.github.com>
2025-05-07 08:30:24 +0530

    new navbar section added

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -49,6 +49,13 @@ export default function Navbar() {
             <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-amber-800 group-hover:w-full transition-all duration-300"></span>
           </Link>
           <Link
+            href="/customization-guide"
+            className="text-gray-700 hover:text-amber-800 relative group transition-colors"
+          >
+            Guide
+            <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-amber-800 group-hover:w-full transition-all duration-300"></span>
+          </Link>
+          <Link
             href="/about"
             className="text-gray-700 hover:text-amber-800 relative group transition-colors"
           >
@@ -197,6 +204,13 @@ export default function Navbar() {
               className="text-gray-700 hover:text-amber-800 relative group transition-colors py-1"
             >
               Collections
+              <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-amber-800 group-hover:w-full transition-all duration-300"></span>
+            </Link>
+            <Link
+              href="/customization-guide"
+              className="text-gray-700 hover:text-amber-800 relative group transition-colors py-1"
+            >
+              Guide
               <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-amber-800 group-hover:w-full transition-all duration-300"></span>
             </Link>
             <Link
